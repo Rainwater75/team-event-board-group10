f5abec6a0c84ffd4a5526f443e2241f23d37143a
John Park <johnjungpark@gmail.com>
2026-04-10 10:22:48 -0400

    feat: add event type

---
+++ src/repository/EventRepository.ts
@@ -0,0 +1,16 @@
+import { Result } from "../lib/result.js";
+
+
+export type Event = {
+    id: number;
+    title: string;
+    description: string;
+    startDate: Date;
+    location: string;
+    category: string; // update to category object later @Ben
+    public: boolean;
+    maxCapacity: number;
+    organizerId: number;
+    attendingUsers: User[];
+};
+
