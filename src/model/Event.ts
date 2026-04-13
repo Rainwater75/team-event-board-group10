cd2b84b5a609aaafcdb3dd08048261e90450b7d1
John Park <johnjungpark@gmail.com>
2026-04-12 20:02:16 -0400

    refactor: moved EventRepository to Events.ts and add InMemoryEventRepository

---
+++ src/model/Event.ts
@@ -0,0 +1,55 @@
+import { Result } from "../lib/result.js";
+import { EventError } from "../lib/errors.js";
+import { IUserSummary } from "../auth/User.js";
+
+export type Category = "None" | "test1" | "test2" | "test3"; // ben i made this up, you can move and change the category types
+
+export interface IEvent {
+    id: number;
+    title: string;
+    description: string;
+    startDate: Date;
+    location: string;
+    category: Category; 
+    public: boolean;
+    maxCapacity: number;
+    organizerId: string; // CHANGE THIS TO UUID
+    attendingUsers: IUserSummary[];
+};
+
+export interface CreateEventInput {
+    title: string;
+    description: string;
+    startDate: Date;
+    location: string;
+    category?: Category; 
+    maxCapacity: number;
+    public?: boolean;
+};
+
+
+export class Event implements IEvent { 
+    id: number;
+    title: string;
+    description: string;
+    startDate: Date;
+    location: string;
+    category: Category;
+    public: boolean;
+    maxCapacity: number;
+    organizerId: string;
+    attendingUsers: IUserSummary[];
+
+    constructor(id: number, data: CreateEventInput, organizerId: string) { //change organizerId to UUID
+        this.id = id;
+        this.title = data.title;
+        this.description = data.description;
+        this.startDate = data.startDate;
+        this.location = data.location;
+        this.category = data.category ?? "None";
+        this.public = data.public ?? false;
+        this.maxCapacity = data.maxCapacity ;
+        this.organizerId = organizerId;
+        this.attendingUsers = [];
+    }
+}
--- src/repository/EventRepository.ts
+++ src/repository/EventRepository.ts
@@ -1,30 +1,10 @@
-import { Result } from "../lib/result.js";
+// this is the central repository where the interfaces are defined
+// they will be implemented in memory and prisma
+
 import { EventError } from "../lib/errors.js";
 import { IUserSummary } from "../auth/User.js";
-
-export type Category = "test1" | "test2" | "test3"; // ben i made this up, you can move and change the category types
-
-export type Event = {
-    id: number;
-    title: string;
-    description: string;
-    startDate: Date;
-    location: string;
-    category: Category; 
-    public: boolean;
-    maxCapacity: number;
-    organizerId: string; // CHANGE THIS TO UUID
-    attendingUsers: IUserSummary[];
-};
-
-export type CreateEventInput = {
-    title: string;
-    description: string;
-    startDate: Date;
-    location: string;
-    category?: Category; 
-    maxCapacity: number;
-};
+import { Result } from "../lib/result";
+import { CreateEventInput } from "../model/Event.js";
 
 // functions for event handling, implement in the repository
 export interface IEventRepository {
